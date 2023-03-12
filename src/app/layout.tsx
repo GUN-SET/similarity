cdbb70e0c7fcd8f555535caf3e6efde6221ff93f
X <androysol@gmail.com>
2023-03-12 18:23:44 +0200

    feat: Navbar, theme switch, toastr
    Created a navbar, for it created exit and login buttons, as well as a
    theme switch and react-hot-toast status output.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import {Inter} from 'next/font/google'
 import {cn} from '@/lib/utils'
 import Providers from '@/components/Providers'
+import Navbar from '@/components/Navbar'
+import {Toaster} from '@/components/ui/Toast'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -12,7 +14,14 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 			className={cn('bg-white text-slate-900 antialiased', inter.className)}
 		>
 			<body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
-				<Providers>{children}</Providers>
+				<Providers>
+					{children}
+
+					<Toaster position='bottom-right' />
+
+					{/* @ts-expect-error Server component*/}
+					<Navbar />
+				</Providers>
 
 				{/* Allow for more height on mobile devices */}
 				<div className='h-40 md:hidden' />
